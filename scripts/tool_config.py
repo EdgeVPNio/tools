c6a736a59d6fec40ea3212ccc4c11a13a245a7b1
Ken <kcratie@users.noreply.github.com>
2020-11-19 20:20:11 -0500

    Add simple sync

--- scripts/tool_config.py
+++ scripts/tool_config.py
@@ -1,15 +1,6 @@
-MAPPING = {
-    "master": {
-        "default": "evio-tools-v20.7.2"
-    },
-    "20.7.2": {
-        "default": "evio-tools-v20.7.2"
-    },
-    "default": "evio-tools-v20.7.2"
-}
+
 MAJOR_VER = 20
 MINOR_VER = 11
 REVISION_VER = 0
 CONTROL_VER = 6
 OFFICIAL = False
-
