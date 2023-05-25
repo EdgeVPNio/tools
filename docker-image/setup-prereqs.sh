d68d819badbfd9b6c1c5a3426daffbcbfc1db7e5
Ken Subratie <kcratie@users.noreply.github.com>
2023-05-25 14:22:31 -0400

    Remove obsolete build files

--- docker-image/setup-prereqs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-systemctl mask getty@tty1.service && \
-mkdir -p /opt/evio && \
-cd /opt/evio && \
-python3.8 -m venv venv  && \
-source venv/bin/activate && \
-pip3 --cache-dir /var/cache/evio/ install wheel psutil slixmpp requests simplejson ryu pyroute2 && \
-deactivate
